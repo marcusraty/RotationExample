0440aa9b698edb1b55247c8325a1812a05d4ff92
marcus <marcusraty@hotmail.com>
2023-12-02 15:33:46 +1000

    Initial Commit

---
+++ RotationExample/RotationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotationExampleApp.swift
+//  RotationExample
+//
+//  Created by Marcus Raty on 2/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RotationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
